104724086f0abf773cda1982c9a09f723b4b05fd
Shanwi5 <shanwibm@gmail.com>
2026-07-01 21:00:15 +0530

    Deploy-ready updates

--- smart_education_system-main/smarteducation/smarteducation/wsgi.py
+++ smart_education_system-main/smarteducation/smarteducation/wsgi.py
@@ -12,5 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smarteducation.settings')
+if os.environ.get('DJANGO_ENV') == 'production':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smarteducation.production')
 
 application = get_wsgi_application()
